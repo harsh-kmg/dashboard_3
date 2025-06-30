ec783a237e77ad448fb9821517fbdf4d42ad77f7
harsh-kmg <harsh_kmg@outlook.com>
2025-06-30 10:50:02 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -311,10 +311,10 @@ def get_filtered_data(FILTER_MONTH,FILTE_YEAR,FILTER_SHAPE,FILTER_COLOR,FILTER_B
     #                                     (master_df['Shape key'] == FILTER_SHAPE) |\
     #                                     (master_df['Color Key'] == FILTER_COLOR) |\
     #                                     (master_df['Buckets'] == FILTER_BUCKET)]
-    filter_data=master_df[(master_df['Month'] == FILTER_MONTH) | \
-                                      (master_df['Year'] == FILTE_YEAR) | \
-                                        (master_df['Shape key'] == FILTER_SHAPE) |\
-                                        (master_df['Color Key'] == FILTER_COLOR) |\
+    filter_data=master_df[(master_df['Month'] == FILTER_MONTH) & \
+                                      (master_df['Year'] == FILTE_YEAR) & \
+                                        (master_df['Shape key'] == FILTER_SHAPE) &\
+                                        (master_df['Color Key'] == FILTER_COLOR) &\
                                         (master_df['Buckets'] == FILTER_BUCKET)]
     try:
         max_buying_price = filter_data['Max Buying Price'].max()
