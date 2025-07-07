d9c1e72d02cb2f27940be4f2b257497de531e3c6
harsh-kmg <harsh_kmg@outlook.com>
2025-07-07 22:45:57 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -412,6 +412,37 @@ def main():
     st.set_page_config(page_title="Yellow Diamond Dashboard", layout="wide")
     st.title("Yellow Diamond Dashboard")
     st.markdown("Upload Excel files to process multiple sheets and filter data.")
+    st.markdown("""
+<style>
+[data-testid="metric-container"] {
+    background-color: #FFF;
+    border: 1px solid #CCC;
+    padding: 5% 5% 5% 10%;
+    border-radius: 5px;
+    border-left: 0.5rem solid #9AD8E1;
+    box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15);
+}
+
+[data-testid="metric-container"] > div {
+    width: fit-content;
+    margin: auto;
+}
+
+[data-testid="metric-container"] label {
+    width: fit-content;
+    margin: auto;
+    color: #36b9cc;
+    font-size: 18px !important;  /* Label size */
+}
+
+[data-testid="metric-container"] > div > div {
+    width: fit-content;
+    margin: auto;
+    font-size: 16px !important;  /* Value size */
+    color: #1f77b4;
+}
+</style>
+""", unsafe_allow_html=True)
     # Initialize session state
     if 'data_processed' not in st.session_state:
         st.session_state.data_processed = False
