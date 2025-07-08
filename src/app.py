3a68d1fc6bb47801529f6844035c5ce1d1a6a905
harsh-kmg <harsh_kmg@outlook.com>
2025-07-08 13:13:15 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -205,7 +205,7 @@ def populate_quarter(df):
     df['Quarter'] = df['Month'].apply(get_quarter)
     return df
 def create_shape_key(x):
-    if x.__contains__(r'HEART'):
+    if x.__contains__(r'HEART MODIFIED BRILLIANT'):
         return 'Other'
     elif x.__contains__(r'CUSHION'):
         return 'Cushion'
